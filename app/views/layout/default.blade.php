ea2f0aa4d178356830398f2740aab5527f534fe6
Panith <panitabiboy@gmail.com>
2014-06-19 19:54:41 +0700

    Removed Space

--- app/views/layout/default.blade.php
+++ app/views/layout/default.blade.php
@@ -10,11 +10,7 @@
 		<title>{{ $title }}</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
 		<!-- Favicons -->
-
-
-
 		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/icons/apple-touch-icon-144-precomposed.png">
 		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/icons/apple-touch-icon-114-precomposed.png">
 		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/icons/apple-touch-icon-72-precomposed.png">
